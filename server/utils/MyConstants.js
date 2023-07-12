c880350fabfe7c074efa08f27a6156237c602921
PerryNguyen <duynguyen.200902@gmail.com>
2023-07-12 09:01:34 +0700

    Add new project food

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+    DB_SERVER : 'cluster0.nrwq9d6.mongodb.net',
+    DB_USER : 'db_perrynguyen',
+    DB_PASS : 'db_20092002',
+    DB_DATABASE : 'shoppingonline',
+    EMAIL_USER : 'duynguyen.200902@gmail.com', // Microsoft mail service
+    EMAIL_PASS : 'snkuhpgnqguitmoo',
+    JWT_SECRET : '<jwt_secret >',
+    JWT_EXPIRES : '6000000', // in milliseconds
+};
+module . exports = MyConstants ;
+    
